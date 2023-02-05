49a1f18a9e17b6c35c816885965e5fcbf5b5dc50
Biagio Ricci <biagior00@gmail.com>
2023-02-05 15:13:37 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Biagio Ricci on 05/02/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
